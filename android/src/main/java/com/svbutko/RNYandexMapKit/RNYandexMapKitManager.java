bbf662f799371760f2c149903669b218166557b4
svbutko <svbutko@hotmail.com>
2019-05-08 16:42:58 +0300

    Change the key

--- android/src/main/java/com/svbutko/RNYandexMapKit/RNYandexMapKitManager.java
+++ android/src/main/java/com/svbutko/RNYandexMapKit/RNYandexMapKitManager.java
@@ -12,7 +12,7 @@ import com.yandex.mapkit.mapview.MapView;
 public class RNYandexMapKitManager extends SimpleViewManager<MapView> {
     public static final String REACT_CLASS = "RNYandexMapKit";
 
-    private static final String MAPKIT_API_KEY = "844d5468-fb6f-437c-92cf-2fb6c6a780d1";
+    private static final String MAPKIT_API_KEY = "216c53c2-0add-46e9-bdcf-fa4d8c960b95";
     private static final Point TARGET_LOCATION = new Point(59.945933, 30.320045);
 
     @Override
