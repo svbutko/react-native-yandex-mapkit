eb89a48bdc129a74cb8fe36f8bcbe1d19481923b
svbutko <svbutko@hotmail.com>
2019-06-26 17:29:54 +0300

    Add ReactModule

--- android/src/main/java/com/svbutko/RNYandexMapKit/RNYandexMapKitModule.java
+++ android/src/main/java/com/svbutko/RNYandexMapKit/RNYandexMapKitModule.java
@@ -4,7 +4,9 @@ package com.svbutko.RNYandexMapKit;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
 
+@ReactModule(name = RNYandexMapKitModule.REACT_CLASS)
 public class RNYandexMapKitModule extends ReactContextBaseJavaModule {
   public static final String REACT_CLASS = "RNYandexMapKit";
   private final ReactApplicationContext reactContext;
